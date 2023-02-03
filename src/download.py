8d003129cce0884b0f72eb0d859b0ea6fdd8e929
trickerer01 <onlysuffering+1@gmail.com>
2023-02-03 11:42:03 +0700

    rev 124: Add logging of scanning results

--- src/download.py
+++ src/download.py
@@ -168,6 +168,7 @@ def scan_dest_folder(dest_base: str) -> None:
     global found_filenames_all
 
     if path.exists(dest_base):
+        Log.info('Scanning dest folder...')
         subfolders = list()
         cur_names = listdir(dest_base)
         for idx_c in reversed(range(len(cur_names))):
@@ -183,6 +184,9 @@ def scan_dest_folder(dest_base: str) -> None:
                 fullpath_s = f'{subfolder}{sub_name}'
                 if path.isfile(fullpath_s):
                     found_filenames_all.add(sub_name)
+        Log.info(f'Found {len(found_filenames_base):d} files in base and '
+                 f'{len(found_filenames_all) - len(found_filenames_base):d} files in {len(subfolders):d} subfolders '
+                 f'(total files: {len(found_filenames_all):d})')
 
 
 def file_exists_in_folder(dest_base: str, idi: int, quality: str, check_subfolders: bool) -> bool:
