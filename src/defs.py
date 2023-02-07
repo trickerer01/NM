5cb1b8ebc56142ba0c700902fae4b09eeedb4be1
trickerer01 <onlysuffering+1@gmail.com>
2023-02-07 20:41:01 +0700

    rev 148: Update user-agent

--- src/defs.py
+++ src/defs.py
@@ -59,7 +59,7 @@ SITE_PAGE_REQUEST_BASE = b64decode(
 # Params required: (int). Ex. SITE_ITEM_REQUEST_BASE % (69999)
 SITE_ITEM_REQUEST_BASE = b64decode('aHR0cHM6Ly93d3cubmF1Z2h0eW1hY2hpbmltYS5jb20vdmlkZW8vJWQv').decode()
 
-USER_AGENT = 'Mozilla/5.0 (X11; Linux i686; rv:68.9) Gecko/20100101 Goanna/4.8 Firefox/68.9'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux i686; rv:102.0) Gecko/20100101 Firefox/102.0'
 DEFAULT_HEADERS = {'User-Agent': USER_AGENT, 'Referer': SITE}
 HOST = urlparse(SITE).netloc
 
