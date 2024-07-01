6d80c648805fce31ede5961c096020376ee37346
trickerer01 <onlysuffering+1@gmail.com>
2024-07-01 18:42:54 +0700

    Fix `convert_extra_tag_for_text_matching()` again

--- src/version.py
+++ src/version.py
@@ -9,7 +9,7 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'NM'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '8'
-APP_REVISION = '408'
+APP_REVISION = '409'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB}.{APP_REVISION}'
 
 #
