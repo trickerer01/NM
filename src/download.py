e6f8039f1fb344203b5d70d6c1932ac00c21bf9c
trickerer01 <onlysuffering+1@gmail.com>
2025-11-05 21:19:11 +0700

    Fix `proxy` vs `no proxy` mode selection logic with `html_without_proxy` and `download_without_proxy`
    (cherry picked from RV commit 27741a13db4e87e3a35b27b2fe60d3b6f8bb1434)

--- src/download.py
+++ src/download.py
@@ -80,8 +80,7 @@ async def scan_video(vi: VideoInfo) -> DownloadResult:
         return DownloadResult.FAIL_NOT_FOUND
 
     vi.set_state(VideoInfo.State.SCANNING)
-    hkwargs = {'noproxy': not Config.proxy or Config.html_without_proxy}
-    a_html = await fetch_html(SITE_ITEM_REQUEST_VIDEO % vi.id, **hkwargs)
+    a_html = await fetch_html(SITE_ITEM_REQUEST_VIDEO % vi.id)
     if a_html is None:
         Log.error(f'Error: unable to retreive html for {sname}! Aborted!')
         return DownloadResult.FAIL_SKIPPED if Config.aborted else DownloadResult.FAIL_RETRIES
@@ -374,13 +373,13 @@ async def download_video(vi: VideoInfo) -> DownloadResult:
                 break
 
             hkwargs: dict[str, dict[str, str]] = {'headers': {'Range': f'bytes={file_size:d}-'} if file_size > 0 else {}}
-            ckwargs = {'allow_redirects': bool(not Config.proxy or not Config.download_without_proxy)}
-            ckwargs.update(noproxy=bool(Config.html_without_proxy and not ckwargs['allow_redirects']))
+            ckwargs = {'allow_redirects': not (Config.proxy and (Config.download_without_proxy or Config.html_without_proxy))}
+            ckwargs.update({'noproxy': bool(Config.proxy and Config.html_without_proxy)})
             hkwargs['headers'].update({'Referer': SITE_ITEM_REQUEST_VIDEO % vi.id})
             r = await wrap_request('GET', vi.link, **ckwargs, **hkwargs)
             while r.status in (301, 302):
                 if urllib.parse.urlparse(r.headers['Location']).hostname != urllib.parse.urlparse(vi.link).hostname:
-                    ckwargs.update(noproxy=True, allow_redirects=True)
+                    ckwargs.update({'noproxy': Config.download_without_proxy, 'allow_redirects': True})
                 ensure_conn_closed(r)
                 r = await wrap_request('GET', r.headers['Location'], **ckwargs, **hkwargs)
             content_len: int = r.content_length or 0
